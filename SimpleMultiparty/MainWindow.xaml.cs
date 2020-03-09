bd0c32c46dd5acb8e822eac7dac3223f70686ac6
josego <jose.garciaorozco@telefonica.com>
2020-03-09 18:40:40 +0100

    make changes for first testeable ingestator with up to 3 cameras in TCP

--- SimpleMultiparty/MainWindow.xaml.cs
+++ SimpleMultiparty/MainWindow.xaml.cs
@@ -11,38 +11,33 @@ namespace SimpleMultiparty
     /// </summary>
     public partial class MainWindow : Window
     {
-        public const string API_KEY = "";
-        public const string SESSION_ID = "";
-        public const string TOKEN = "";
+        public const string API_KEY = "46464702";
+        //TCP
+         public const string SESSION_ID = "1_MX40NjQ2NDcwMn5-MTU4Mzc3NDY5MTU4NH5pMXRqcXNkYlM5NVVGb1NIK1Y2QUdDMmR-fg";
+        public const string TOKEN = "T1==cGFydG5lcl9pZD00NjQ2NDcwMiZzaWc9MjQyY2NmNmYyMDBjMzBiZGEzNThlN2Q2MWNjMTU4YWUzYTM5NDRhYjpzZXNzaW9uX2lkPTFfTVg0ME5qUTJORGN3TW41LU1UVTRNemMzTkRZNU1UVTROSDVwTVhScWNYTmtZbE01TlZWR2IxTklLMVkyUVVkRE1tUi1mZyZjcmVhdGVfdGltZT0xNTgzNzc0Nzk5Jm5vbmNlPTAuMDI3NjI1MDUzMDYyOTk0ODk4JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE1ODQzNzk1OTkmY29ubmVjdGlvbl9kYXRhPXh0cmVhbXJVc2VySWQlM0QwJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+            //UDP
+            //  public const string SESSION_ID = "1_MX40NjI3MDc3Mn5-MTU4Mzc1NDQ5Mjg0OH5tVUJaUXdIdWNjejZNeUpBSllseVY1NUN-UH4";
+            //  public const string TOKEN = "T1==cGFydG5lcl9pZD00NjI3MDc3MiZzaWc9Y2IwNGMwMjY2NDc4MGQwNDA4MzI0NTBjOGQyZTlhNTE1NTAzMzU4NTpzZXNzaW9uX2lkPTFfTVg0ME5qSTNNRGMzTW41LU1UVTRNemMxTkRRNU1qZzBPSDV0VlVKYVVYZElkV05qZWpaTmVVcEJTbGxzZVZZMU5VTi1VSDQmY3JlYXRlX3RpbWU9MTU4Mzc1ODE2MyZub25jZT0wLjgxMzcyMzg2Njk2ODI4MDcmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU4NDM2Mjk2MyZjb25uZWN0aW9uX2RhdGE9eHRyZWFtclVzZXJJZCUzRDAmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=";
 
-        VideoCapturer Capturer;
         Session Session;
-        Publisher Publisher;
+        Session UDPSession;
+        IList<VideoCapturer.VideoDevice> devices;
+        List<Publisher> Publisers = new List<Publisher>();
+        List<VideoCapturer> Capturers = new List<VideoCapturer>();
+
+        //   Publisher Publisher;
         bool Disconnect = false;
         Dictionary<Stream, Subscriber> SubscriberByStream = new Dictionary<Stream, Subscriber>();
 
         public MainWindow()
         {
             InitializeComponent();
-
-            // This shows how to enumarate the available capturer devices on the system to allow the user of the app
-            // to select the desired camera. If a capturer is not provided in the publisher constructor the first available 
-            // camera will be used.
-            var devices = VideoCapturer.EnumerateDevices();
-            if (devices.Count > 0)
-            {
-                var selectedDevice = devices[0];
-                Trace.WriteLine("Using camera: " + devices[0].Name);
-                Capturer = selectedDevice.CreateVideoCapturer(VideoCapturer.Resolution.High);
-            }
-            else
-            {
-                Trace.WriteLine("Warning: no cameras available, the publisher will be audio only.");
-            }
-
+            devices = VideoCapturer.EnumerateDevices();
+            FillSelectorWithWebCams();
+            AddPublisherButton.IsEnabled = false;
             // We create the publisher here to show the preview when application starts
             // Please note that the PublisherVideo component is added in the xaml file
-            Publisher = new Publisher(Context.Instance, renderer: PublisherVideo, capturer: Capturer);
+           // Publisher = new Publisher(Context.Instance, renderer: PublisherVideo, capturer: Capturer, name: "CAMERA");
 
             if (API_KEY == "" || SESSION_ID == "" || TOKEN == "")
             {
@@ -64,14 +59,61 @@ namespace SimpleMultiparty
             Closing += MainWindow_Closing;
         }
 
+
+        private void FillSelectorWithWebCams()
+        {
+            foreach (VideoCapturer.VideoDevice device in devices) {
+                WebcamSelector.Items.Add(device.Name);
+            }
+           
+        }
+
+        private void Publish_Click(object sender, RoutedEventArgs e)
+        {
+            int position = WebcamSelector.SelectedIndex;
+
+            var Capturer = devices[position].CreateVideoCapturer(VideoCapturer.Resolution.High);
+
+            var publisherRenderer = PublisherVideo_1;
+            switch (Publisers.Count) {
+                case 0:
+                    publisherRenderer = PublisherVideo_1;
+                    break;
+                case 1:
+                    publisherRenderer = PublisherVideo_2;
+                    break;
+                default:
+                    publisherRenderer = PublisherVideo_3;
+                    break;
+            }
+
+            var Publisher = new Publisher(Context.Instance, renderer: publisherRenderer, capturer: Capturer,name:"CAMERA");
+            Publisers.Add(Publisher);
+            Capturers.Add(Capturer);
+
+            Session.Publish(Publisher);
+        }
+
+        private void UnSuscribeAll() {
+            foreach (var myPublisers in Publisers)
+            {
+                myPublisers?.Dispose();
+            }
+            Publisers.Clear();
+            foreach (var myCapturers in Capturers)
+            {
+                myCapturers?.Dispose();
+            }
+            Capturers.Clear();
+        }
+
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             foreach (var subscriber in SubscriberByStream.Values)
             {
                 subscriber.Dispose();
             }
-            Publisher?.Dispose();
-            Capturer?.Dispose();
+            UnSuscribeAll();
             Session?.Dispose();
         }
 
@@ -79,7 +121,7 @@ namespace SimpleMultiparty
         {
             try
             {
-                Session.Publish(Publisher);
+                AddPublisherButton.IsEnabled = true;
             }
             catch (OpenTokException ex)
             {
@@ -149,6 +191,13 @@ namespace SimpleMultiparty
             }
         }
 
+        private void UnpublishAll() {
+            foreach (var myPublisers in Publisers)
+            {
+                Session.Unpublish(myPublisers);
+            }
+        }
+
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
             if (Disconnect)
@@ -156,8 +205,10 @@ namespace SimpleMultiparty
                 Trace.WriteLine("Disconnecting session");
                 try
                 {
-                    Session.Unpublish(Publisher);
-                    Session.Disconnect();
+                    //     UnpublishAll();
+                    //     Session.Disconnect();
+                    UnSuscribeAll();
+                    Session?.Dispose();
                 }
                 catch (OpenTokException ex)
                 {
